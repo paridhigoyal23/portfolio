cfa7264bd7fb76b617d688716abc57694daa47a9
Paridhi <paridhigoyal23@gmail.com>
2025-04-18 22:41:01 +0530

    portfolio completed

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Web Developer"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const btnToggle = document.getElementById("btnToggle");
-  const navbar = document.getElementById("navbar");
-
-  btnToggle.addEventListener("click", function () {
-    // Toggle the 'active' class on the navigation bar
-    navbar.classList.toggle("active");
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
   });
-});
-document.addEventListener("DOMContentLoaded", function () {
-  var scrollLinks = document.querySelectorAll(".scroll-link");
-
-  scrollLinks.forEach(function (scrollLink) {
-    scrollLink.addEventListener("click", function (event) {
-      event.preventDefault();
-
-      var targetId = this.getAttribute("href");
-      var targetElement = document.querySelector(targetId);
-
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: "smooth",
-        });
-      }
+  document.addEventListener("DOMContentLoaded", function () {
+    const btnToggle = document.getElementById("btnToggle");
+    const navbar = document.getElementById("navbar");
+  
+    btnToggle.addEventListener("click", function () {
+      // Toggle the 'active' class on the navigation bar
+      navbar.classList.toggle("active");
     });
   });
-});
+  document.addEventListener("DOMContentLoaded", function () {
+    var scrollLinks = document.querySelectorAll(".scroll-link");
+  
+    scrollLinks.forEach(function (scrollLink) {
+      scrollLink.addEventListener("click", function (event) {
+        event.preventDefault();
+  
+        var targetId = this.getAttribute("href");
+        var targetElement = document.querySelector(targetId);
+  
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: "smooth",
+          });
+        }
+      });
+    });
+  });
